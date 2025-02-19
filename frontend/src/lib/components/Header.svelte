21a3379efdd32b9b938218c557164135cd9cd1dc
catpho <cathyp98@outlook.com>
2025-02-19 18:41:43 -0500

    nav

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -5,6 +5,7 @@
 	import { authStore, authHandlers } from '$lib/stores/authStore';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
+	import { Avatar, Dropdown, DropdownHeader, DropdownItem, DropdownDivider } from 'flowbite-svelte';
 	import { userStore } from '$lib/stores/userStore';
 	import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
@@ -14,9 +15,10 @@
 	let uid;
 	let profileImage;
 	let imageUrl = '';
+	let displayName = '';
 
-	let pathname;
-	$: pathname = $page.url.pathname;
+	// let pathname;
+	// $: pathname = $page.url.pathname;
 
 	export let register;
 
@@ -29,6 +31,7 @@
 	userStore.subscribe((curr) => {
 		uid = curr?.currentUser?.uid;
 		profileImage = curr?.currentUser?.profileImage;
+		displayName = curr?.currentUser?.displayName;
 	});
 
 	$: {
@@ -85,45 +88,26 @@
 <!-- make it so the profile pic is the all destination -->
 <header class=" font-tanaegean flex h-20 w-full items-center justify-between bg-white p-5">
 	<div class="app flex w-full flex-row items-center justify-between">
-		{#if pathname === '/privateDashboard'}
+		<!-- {#if pathname === '/privateDashboard'}
 			<h1 class="mr-auto items-center text-lg font-bold">My Notes</h1>
-		{/if}
+		{/if} -->
 		<nav class="ml-auto flex flex-row items-center justify-center gap-10 rounded-full bg-white">
 			<ul>
 				<li class=" flex items-center gap-5 font-bold">
 					<!-- This if else statement is what causes the showLoginModal to not allow for a redirect to occur after loggin in / might be due to bubble/capture effect-->
 					{#if email != undefined}
-						<!-- <button type="button" class="whitespace-nowrap rounded-lg px-6 py-2" on:click={logout}
-							>Logout</button
-						> -->
-
+						<Avatar id="user-drop" src={imageUrl} class="cursor-pointer" dot={{ color: 'green' }} />
+						<Dropdown triggeredBy="#user-drop">
+							<DropdownHeader>
+								<span class="block text-sm">{displayName}</span>
+								<span class="block truncate text-sm font-medium">name@flowbite.com</span>
+							</DropdownHeader>
+							<DropdownItem on:click={goHome}>Dashboard</DropdownItem>
+							<DropdownItem on:click={goProfile}>Settings</DropdownItem>
+							<DropdownDivider />
+							<DropdownItem on:click={logout}>Sign out</DropdownItem>
+						</Dropdown>
 						<!-- svelte-ignore a11y_invalid_attribute -->
-						<a aria-label="userProfile" href="" on:click={() => (showDropdown = !showDropdown)}>
-							{#if imageUrl != null}
-								<img
-									class=" display-flex h-10 w-10 rounded-full"
-									src={imageUrl}
-									alt="Profile"
-									referrerPolicy="no-referrer"
-								/>
-							{:else}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="2"
-									stroke="currentColor"
-									class="h-6 w-6"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M12 14c4 0 6 2 6 4v2H6v-2c0-2 2-4 6-4z"
-									/>
-									<circle cx="12" cy="7" r="4" />
-								</svg>
-							{/if}
-						</a>
 					{:else}
 						<button
 							on:click={() => {
@@ -151,21 +135,6 @@
 			</ul>
 		</nav>
 	</div>
-	{#if showDropdown}
-		<div class="absolute right-0 mt-2 w-48 rounded border bg-white shadow-lg">
-			<ul class="p-2">
-				<li>
-					<button on:click={goHome} class="w-full px-4 py-2 text-left">Go to Home</button>
-				</li>
-				<li>
-					<button on:click={goProfile} class="w-full px-4 py-2 text-left">Go to Profile</button>
-				</li>
-				<li>
-					<button on:click={logout} class="w-full px-4 py-2 text-left text-red-500">Logout</button>
-				</li>
-			</ul>
-		</div>
-	{/if}
 </header>
 
 <style>
